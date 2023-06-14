ad0ab7224875e5e85311e2e3d458530eec1425ec
Jane Ladaban <jladaban@squareup.com>
2023-06-14 10:10:43 -0400

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Jane Ladaban on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
