d4bfce0310e3c038a3e20f709bcd9c13ec061f80
Jane Ladaban <jladaban@squareup.com>
2023-06-20 19:07:41 -0400

    adding QuestionView

---
+++ QuestionView.swift
@@ -0,0 +1,51 @@
+//
+//  QuestionView.swift
+//  QuizApp
+//
+//  Created by Jane Ladaban on 6/20/23.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    @Binding var question: QuestionData
+    @State var userAnswer: String = ""
+    
+    var body: some View {
+        VStack {
+            // show question
+            Text(question.question)
+            
+            // show choices
+            ForEach(question.choices, id: \.self) { choice in
+                Button(choice) {
+                    question.isAnswered.toggle() // mark question as answered
+                    userAnswer = choice
+                    
+                    if (userAnswer == question.answer) {
+                        incrementScore()
+                        question.isCorrect.toggle()
+                    }
+                }
+            }
+            
+            // show result
+            if (question.isAnswered) {
+                if (question.isCorrect) {
+                    Text("You got it right! The correct answer is \(question.answer).")
+                        .foregroundColor(.green)
+                } else {
+                    Text("You are incorrect. The correct answer is \(question.answer).")
+                        .foregroundColor(.red)
+                }
+            }
+        }
+        .disabled(question.isAnswered) // disable once answered
+    }
+}
+
+struct QuestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionView(question: .constant(DataModel.testData[0]))
+    }
+}
