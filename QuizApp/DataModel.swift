629b2290919ff53ff0a605c652d9da75b9446e91
Jane Ladaban <jladaban@squareup.com>
2023-06-23 16:05:13 -0400

    initialize isAnswered and isCorrect in struct level

--- QuizApp/DataModel.swift
+++ QuizApp/DataModel.swift
@@ -13,19 +13,19 @@ struct QuestionData: Hashable, Identifiable {
     var question: String
     var choices: [String]
     var answer: String
-    var isAnswered: Bool
-    var isCorrect: Bool
+    var isAnswered: Bool = false
+    var isCorrect: Bool = false
 }
 
 class DataModel: NSObject {
     static let data: [QuestionData] = [
-        QuestionData(id: 1, headline: "New York", question: "What is the capital of New York state?", choices: ["New York City", "Albany", "Queens"], answer: "Albany", isAnswered: false, isCorrect: false),
-        QuestionData(id: 2, headline: "Washington, DC", question: "Is Washington, DC a state?", choices: ["Yes", "No"], answer: "No", isAnswered: false, isCorrect: false)
+        QuestionData(id: 1, headline: "New York", question: "What is the capital of New York state?", choices: ["New York City", "Albany", "Queens"], answer: "Albany"),
+        QuestionData(id: 2, headline: "Washington, DC", question: "Is Washington, DC a state?", choices: ["Yes", "No"], answer: "No")
     ]
     
     static let testData: [QuestionData] = [
-        QuestionData(id: 0, headline: "What do you think?", question: "Do you think this is a test?", choices: ["Yes", "No", "Maybe"], answer: "Maybe", isAnswered: false, isCorrect: false),
-        QuestionData(id: 99, headline: "Is it?", question: "Is this a real question?", choices: ["Yes", "No", "Maybe"], answer: "No", isAnswered: false, isCorrect: false),
+        QuestionData(id: 0, headline: "What do you think?", question: "Do you think this is a test?", choices: ["Yes", "No", "Maybe"], answer: "Maybe"),
+        QuestionData(id: 99, headline: "Is it?", question: "Is this a real question?", choices: ["Yes", "No", "Maybe"], answer: "No"),
     ]
 }
 
