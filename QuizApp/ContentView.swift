cac358c4022887d51e9b90946d29ccec11b8c3af
Jane Ladaban <jladaban@squareup.com>
2023-06-23 16:05:33 -0400

    making the text bigger

--- QuizApp/ContentView.swift
+++ QuizApp/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
                         .imageScale(.large)
                         .foregroundColor(.accentColor)
                     Text("USA Facts Quiz")
+                        .font(.title)
                 }
                 
                 // show CTA
