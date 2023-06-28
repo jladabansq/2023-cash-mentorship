d6a738138b704aac3113681ad66cf5a9abb7c31d
Jane Ladaban <jladaban@squareup.com>
2023-06-28 18:40:38 -0400

    adding styling using custom color set

--- QuizApp/QuestionView.swift
+++ QuizApp/QuestionView.swift
@@ -12,49 +12,57 @@ struct QuestionView: View {
     
     var body: some View {
         VStack {
-            // show question and choices
-            VStack {
-                Text(question.question)
-                    .font(.title3)
-                    .fixedSize(horizontal: false, vertical: true)
+            ZStack {
+                // background
+                LinearGradient(gradient: Gradient(colors: [Color("cp-3"), Color("cp-2"), Color("cp-1")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
                 
-                ForEach(question.choices, id: \.self) { choice in
-                    Button(choice) {
-                        question.isAnswered.toggle() // mark question as answered
-                        question.userAnswer = choice
+                // everything else
+                VStack {
+                    // show question and choices
+                    VStack {
+                        Text(question.question)
+                            .font(.title3)
+                            .fixedSize(horizontal: false, vertical: true)
                         
-                        if (question.userAnswer == question.answer) {
-                            incrementScore()
-                            question.isCorrect.toggle()
+                        ForEach(question.choices, id: \.self) { choice in
+                            Button(choice) {
+                                question.isAnswered.toggle() // mark question as answered
+                                question.userAnswer = choice
+                                
+                                if (question.userAnswer == question.answer) {
+                                    incrementScore()
+                                    question.isCorrect.toggle()
+                                }
+                            }
+                            .bold(question.isAnswered && choice == question.answer ? true : false)
+                            .padding()
+                            .foregroundColor(question.isAnswered && choice == question.answer ? Color("cp-green") : .primary) // if that choice is not the correct answer, then put no background
                         }
                     }
                     .padding()
-                    .buttonStyle(.bordered)
-                    .foregroundColor(question.isAnswered && choice == question.answer ? .green : .black)
-                }
-            }
-            .padding()
-            .border(.gray, width: 3)
-            
-            // show result if answered
-            VStack {
-                if (question.isAnswered) {
-                    HStack {
-                        Image(systemName: question.isCorrect ? "person.fill.checkmark" : "")
-                        Text(question.isCorrect ? "You got it right!" : "\(question.userAnswer) is incorrect.")
-                    }
-                    .padding(.bottom, 10)
-                    .foregroundColor(question.isCorrect ? .green : .red)
+                    .border(.primary, width: 1)
                     
-                    ScrollView {
-                        Text(question.explanation)
-                        
+                    // show result if answered
+                    VStack {
+                        if (question.isAnswered) {
+                            HStack {
+                                Image(systemName: question.isCorrect ? "person.fill.checkmark" : "person.fill.xmark")
+                                Text(question.isCorrect ? "You got it right!" : "\(question.userAnswer) is incorrect.")
+                            }
+                            .padding(.bottom, 10)
+                            .foregroundColor(question.isCorrect ? Color("cp-green") : Color("cp-red"))
+                            
+                            ScrollView {
+                                Text(question.explanation)
+                                
+                            }
+                        }
                     }
+                    .padding()
                 }
             }
-            .padding(.top, 30)
+            
         }
-        .padding()
         .disabled(question.isAnswered) // disable if answered
     }
 }
